59e320d08baf183eb47c83f9577334017b70b181
Jothiswaran Arumugam <53011406+devjothish@users.noreply.github.com>
2023-12-26 06:43:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,7 @@ const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
   question.innerHTML = "Nahhh, this is !!! 2 more days for 4 months🥹";
-  gif.src =
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWpoMW1iem1pN2N6MHlvZ3d2cXgzMWo0czZrOHd3YzJycnA4NTBmcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/BxUJ8T9oMzFcsE4dAv/giphy.gif";
+  gif.src = "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExYWpoMW1iem1pN2N6MHlvZ3d2cXgzMWo0czZrOHd3YzJycnA4NTBmcSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/BxUJ8T9oMzFcsE4dAv/giphy.gif";
 });
 
 noBtn.addEventListener("mouseover", () => {
